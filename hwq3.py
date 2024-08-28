478e47df797853646f5e05cabf7e89b3e9993059
Daniel Kassaye <danielkassaye@daniels-air-4.mynetworksettings.com>
2024-08-27 21:33:25 -0700

    second commit

--- hwq3.py
+++ hwq3.py
@@ -1,20 +1,28 @@
+# 4. Arithmetic Operations in Daily Life
 
- # 3. Python Data Types and type() Function
+ # Objective: The aim of this assignment is to get familiarized with basic arithmetic operations 
+ # and understand how they can be applied in everyday situations.
 
- # Objective: The aim of this assignment is to practice identifying and using Python's basic data types, 
- # as well as the type() function for checking a variable's data type.
+ # Task 1: Grocery Store Math
+ # Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. 
+ # For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic
 
- # Your task is to identify the data type of each variable and then use the type() function to print it out:
+ #bread can be 8 dollars, peanut butter 5 dollars, and jelly 7 dollars. 
 
- # have put in print statements for type() of variable
-variable_a = "Hello, World!"
-print(type(variable_a))
+total_cost = 0
+bread_price = 8
+pb_price = 5
+jelly_price = 7
+total_cost = bread_price + pb_price + jelly_price
+print(total_cost)
 
-variable_b = 23
-print(type(variable_b))
 
-variable_c = 3.14
-print(type(variable_c))
 
-variable_d = True
-print(type(variable_d)) 
+ # Task 2: Bank Interest
+ # If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the 
+ # total amount in your account after a year. So if you had $10,000 at a 7% interest write code that would tell 
+ # us the amount at the end of the year. For the example the expected outcome would be $10,700.
+start_balance = 10000
+int_rate = 0.07
+end_balance = start_balance + (start_balance * int_rate)
+print(end_balance)
