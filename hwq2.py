71894e4ada763dcf24ee77b7c08a67cb64c3e602
Daniel Kassaye <danielkassaye@daniels-air-4.mynetworksettings.com>
2024-09-04 18:55:30 -0700

    third commit

--- hwq2.py
+++ hwq2.py
@@ -6,13 +6,13 @@
  # Edit done
 
  #This is a Pi value which must be written in ALL caps form since it is a constant. 
-PIVALUE = 3.14
+PI_VALUE = 3.14
 
  #camelCase
 userAge = 25
  #snake_case
 user_location = "New York"
  #This is a constant since it is in ALL Caps. Leave it alone
-MAXLIMIT = 1000
+MAX_LIMIT = 1000
 
 
--- notes.py
+++ notes.py
@@ -1,41 +1,95 @@
-'''
+# Python Syntax
+# camelCase, snake_case, PascalCase, UPPER_CASE
+firstName = 'christian'
+first_name = ''
+FirstName = 'christian' # used only for classes
+FIRST_NAME = 'christian' # used only for constants (value never changes) 
 
-This is a note for Module 5: Relational Databases
-This is a Multi-line String comment. 
+# Python Conditionals (if, elif, else)
+# falsy values
+# empty sequences and collections (lists, tuples, strings, dictionaries, sets), zero in every numeric type, None, and False
 
-LESSON 1: Database Fundamentals
+temp = 65
 
-'''
-#Database is a file that is organized for storing data. 
-
-# Relational Database --> a database structured to recognize relations among stored items of information.
-# Data is structured in rows and columns
-# order reigns supreme
-# Primary Keys are unique
-#-----------------------------------------------------------------------------------------------------------------------------
-# Non-Relational Database --> Mix of structures, Data lounging in documents, key-value pairs, and wide columns that stretch as
-# far as the eye can see. 
-# Provides more flexibility
-
-#-----------------------------------------------------------------------------------------------------------------------------
-# Relational Database --> Tables --> Rows --> Columns  
-
-# Tables --> Rows and Columns 
-
-#Intro to Keys -- Primary and Foriegn Keys with Indexing
-
-# Constraints --> Unique and Non-Null
-# Default Value
-
-#-----------------------------------------------------------------------------------------------------------------------------
-#Database Management Systems (DBMS) -->
-# MySQL
-# PostgreSQL
-# Oracle
-# SQLite
+if (temp):
+    print('temp defined')
+else:
+    print('temp undefined')
 
 
-#Entity-Relationship Diagrams (ERDs) --> like maps (of a city)
-# Components of ERD --> Entities, Attributes, and Relationships
-x = 1 
-print(x)
+
+# comparison operators
+# <, <=, >, >=, ==, !=
+if (temp >= 90):
+    print('Its pretty steamy outside, do not wear black!')
+elif (temp >= 75):
+    print('Its a nice day!')
+elif (temp == 65):
+    print('Its exactly 65 degrees')
+else:
+    print('Its coooooold outside, consider laying up!')
+
+
+# logical AND & OR
+# nested if & input function
+
+username = ''
+password = 'gohawks'
+
+if (first_name == 'christian' or username == 'csachs'):
+    user = input('Are you the Lead Instructor, Christian Sachs?(yes or no)\n').lower()
+    if (user == 'yes'):
+        password = input('What is your password?\n')
+        if (password == 'gohawks'):
+            print('Access Granted: Hello ' + username)
+        else:
+            print('Incorrect password, try again')
+    else:
+        print('Hello, Student!')
+else:
+    print('Hello, Student!')
+
+
+# Python Lists: Stores multiple values in a single variable
+students = ['Bdada', 'Dennis', 'Brittany', 'Sakura']
+
+# indexing
+print(students[0])
+print(students[1])
+print(students[2])
+
+
+# slicing (start(includes): stop(excludes): step)
+print(students[:2])
+print(students[1::2])
+
+passing_students = students[0:2]
+print(passing_students)
+
+
+# add to list
+students.append('Mason')
+print(students)
+
+# remove from list using .pop(0), .remove('student 1'), del, .clear()
+students.pop(2) # .pop(2) removes index value
+print(students)
+
+students.remove('Bdada') # removes the value itself
+print(students)
+
+del students[0] # deletes the index defined
+print(students)
+
+students.clear() # clears all values in a list
+print(students)
+
+
+# membership checks with "in" & "not in"
+
+languages = ['python', 'html/css', 'javascript']
+
+if ('c++' not in languages):
+    print('Language is NOT in the program')
+else:
+    print('Language is apart of the curriculum')
